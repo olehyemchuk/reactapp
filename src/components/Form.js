6ca8c8205cf58aefc313b3f55addcb393f1ea920
Oleh <emchuk86@gmail.com>
2020-04-30 16:38:25 +0300

    added components note, alert, form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import { AlertContext } from "./../context/alert/alertContext";
 
 export const Form = () => {
+  const [value, setValue] = useState("");
+  const alert = useContext(AlertContext);
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    if (value.trim()) {
+      alert.show("note was created", "success");
+      setValue("");
+    } else {
+      alert.show("Enter note name!");
+    }
+  };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="form-group">
         <input
           className="form-control"
           type="text"
           placeholder="Name of note"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
         ></input>
       </div>
     </form>
--- src/components/Notes.js
+++ src/components/Notes.js
@@ -4,8 +4,14 @@ export const Notes = ({ notes }) => {
   return (
     <ul className="list-group">
       {notes.map((note) => (
-        <li className="list-group-item" key={note.id}>
-          {note.title}
+        <li className="list-group-item note" key={note.id}>
+          <div>
+            <strong>{note.title}</strong>
+            <span>{new Date().toLocaleDateString()}</span>
+          </div>
+          <button type="button" className="btn btn-outline-danger btn-sm">
+            &times;
+          </button>
         </li>
       ))}
     </ul>
